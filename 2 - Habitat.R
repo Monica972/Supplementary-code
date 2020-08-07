bc10616960ae9fe049a991732e35edf41e025321
Monica972 <58348905+Monica972@users.noreply.github.com>
2020-08-07 07:20:10 -0700

    Update 2 - Habitat.R

--- 2 - Habitat.R
+++ 2 - Habitat.R
@@ -1,10 +1,10 @@
 # Set working directory
 
-setwd("~/Documents/Honours")
+setwd("~/Documents/Short")
 getwd()
 
 # Read data file
-use.data<- read.csv("./DATA/Shortdata.csv")
+use.data<- read.csv("./Shortdata.csv")
 
 # Remove any species duplicates
 use_clean <- use.data[!duplicated(use.data),]
@@ -15,19 +15,27 @@ med <- use_clean[which(use_clean$useTradeId..30 =="TRUE"),]
 med <- med[!duplicated(med$scientific_name),]
 obsnum <- nrow(med)
 
+#proportions of species that have known and unknown habitats
+Unkn <- med[which(med$Terrestrial == "FALSE" & med$Freshwater == "FALSE" & med$Marine == "FALSE"),]
+Unknnum <- nrow(Unkn)
+Unknper <- round((Unknnum/obsnum * 100), dig = 2)
+
+Know <- med[which(med$Terrestrial == "TRUE" | med$Freshwater == "TRUE" | med$Marine == "TRUE"),]
+Knownum <- nrow(Know)
+Knowper <- round((Knownum/obsnum * 100), dig = 2)
+
 #Habitat type
 #Terrestrial
 TR <- med[which(med$Terrestrial == "TRUE"),]
 TRnum <- nrow(TR)
-TRper <-round((TRnum/obsnum * 100), dig=2)
+TRper <-round((TRnum/Knownum * 100), dig=2)
 
 #Freshwater
 FW <- med[which(med$Freshwater == "TRUE"),]
 FWnum <- nrow(FW)
-FWper <-round((FWnum/obsnum * 100), dig=2)
+FWper <-round((FWnum/Knownum * 100), dig=2)
 
 #Marine
 MA <- med[which(med$Marine == "TRUE"),]
 MAnum <- nrow(MA)
-MAper <-round((MAnum/obsnum * 100), dig=2)
-
+MAper <-round((MAnum/Knownum * 100), dig=2)
