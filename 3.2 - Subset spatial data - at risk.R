8d5e8a5ed31957c1f155f2d0723e4e05e24e3c89
Monica972 <58348905+Monica972@users.noreply.github.com>
2020-08-13 07:52:06 -0700

    Update 3.2 - Subset spatial data - at risk.R

--- 3.2 - Subset spatial data - at risk.R
+++ 3.2 - Subset spatial data - at risk.R
@@ -13,9 +13,12 @@ library(raster)
 
 ## ------------------------------------------------------------------------------
 
-          ## Repeat following code for each species group
-## Birds, Conus, Lobsters, Reptiles, Marinefish, Amphibians, FW_Groups, Mammals, Seacucumbers, Sharks_Rays_Chimaeras
+#Subset medicinal species to those at risk
 
+## ------------------------------------------------------------------------------
+
+## ------------------------------------------------------------------------------
+# BIRDS
 ## ------------------------------------------------------------------------------
 
 #read in range polygon shapefile, change filepath
@@ -28,3 +31,224 @@ nrow(atRisk)
 atRisk.sp <- as(atRisk, "Spatial")
 
 shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskbirds.shp", overwrite = TRUE)
+
+## ------------------------------------------------------------------------------
+# CONUS
+## ------------------------------------------------------------------------------
+
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("conus.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskconus.shp", overwrite = TRUE)
+
+## ------------------------------------------------------------------------------
+# LOBSTERS
+## ------------------------------------------------------------------------------
+
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("lobs.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Risklobs.shp", overwrite = TRUE)
+
+## ------------------------------------------------------------------------------
+# REPTILES
+## ------------------------------------------------------------------------------
+
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("reptiles.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskreptiles.shp", overwrite = TRUE)
+
+## ------------------------------------------------------------------------------
+# AMPHIBIANS
+## ------------------------------------------------------------------------------
+
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("amphibs.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskamphibs.shp", overwrite = TRUE)
+
+## ------------------------------------------------------------------------------
+# MAMMALS
+## ------------------------------------------------------------------------------
+
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("mammals.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskmammals.shp", overwrite = TRUE)
+
+## ------------------------------------------------------------------------------
+# SEACUCUMBERS
+## ------------------------------------------------------------------------------
+
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("seacucs.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskseacucs.shp", overwrite = TRUE)
+
+## ------------------------------------------------------------------------------
+# SHARKS_RAYS_CHIMAERAS
+## ------------------------------------------------------------------------------
+
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("sharks.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Risksharks.shp", overwrite = TRUE)
+
+## ------------------------------------------------------------------------------
+# FW_GROUPS
+## ------------------------------------------------------------------------------
+
+#PART1
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("fwpt1.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskfwpt1.shp", overwrite = TRUE)
+
+#PART2
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("fwpt2.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskfwpt2.shp", overwrite = TRUE)
+
+#PART3
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("fwpt3.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskfwpt3.shp", overwrite = TRUE)
+
+#PART4
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("fwpt4.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskfwpt4.shp", overwrite = TRUE)
+
+#PART5
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("fwpt5.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskfwpt5.shp", overwrite = TRUE)
+
+#PART6
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("fwpt6.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskfwpt6.shp", overwrite = TRUE)
+
+## ------------------------------------------------------------------------------
+# MARINEFISH
+## ------------------------------------------------------------------------------
+
+#PART1
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("mfpt1.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskmfpt1.shp", overwrite = TRUE)
+
+#PART2
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("mfpt2.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskmfpt2.shp", overwrite = TRUE)
+
+#PART3
+#read in range polygon shapefile, change filepath
+ranges <- read_sf("mfpt3.shp")
+
+#select at-risk spp
+atRisk <- ranges %>% dplyr::filter(`redlistCategoryTitle` == "Vulnerable" | `redlistCategoryTitle` == "Endangered" | `redlistCategoryTitle` == "Critically Endangered")
+nrow(atRisk)
+
+atRisk.sp <- as(atRisk, "Spatial")
+
+shapefile(atRisk.sp, "~/Documents/SpatialData/AtRisk/Riskmfpt3.shp", overwrite = TRUE)
