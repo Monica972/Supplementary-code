f9593e8b5ddeee513fb91cc3a1e5f3ce3075a98b
Monica972 <58348905+Monica972@users.noreply.github.com>
2020-10-23 17:04:56 -0700

    Update 6 - Spiritual vs Non-spiritual Uses.R

--- 6 - Spiritual vs Non-spiritual Uses.R
+++ 6 - Spiritual vs Non-spiritual Uses.R
@@ -25,10 +25,6 @@ library(shades)
 # Read data file
 use.data<- read.csv("./Shortdata.csv")
 
-# Remove any species duplicates
-use_clean <- use.data[!duplicated(use.data),]
-use_clean <- use_clean[!duplicated(use_clean$scientific_name),]
-
 # Create data subset of medicinal use species 
 med <- use_clean[which(use_clean$useTradeId..30 =="TRUE"),]
 med <- med[!duplicated(med$scientific_name),]
